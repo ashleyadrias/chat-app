2dcd9c0b790ee2fa868b1176acf6b8aa57aa1cd8
ashleyadrias <agadrias@gmail.com>
2020-04-03 21:52:26 -0700

    added whitebaord

--- client/src/components/TextContainer/TextContainer.js
+++ client/src/components/TextContainer/TextContainer.js
@@ -4,29 +4,22 @@ import onlineIcon from '../../icons/onlineIcon.png';
 
 import './TextContainer.css';
 
+import WhiteBoard from './WhiteBoard/WhiteBoard';
+import Tools from './WhiteBoard/tools';
+import History from './WhiteBoard/history';
+
 const TextContainer = ({ users }) => (
   <div className="textContainer">
-    <div>
-      <h1>Realtime Chat Application <span role="img" aria-label="emoji">💬</span></h1>
-      <h2>Created with React, Express, Node and Socket.IO <span role="img" aria-label="emoji">❤️</span></h2>
-      <h2>Try it out right now! <span role="img" aria-label="emoji">⬅️</span></h2>
-    </div>
     {
       users
         ? (
-          <div>
-            <h1>People currently chatting:</h1>
-            <div className="activeContainer">
-              <h2>
-                {users.map(({name}) => (
-                  <div key={name} className="activeItem">
-                    {name}
-                    <img alt="Online Icon" src={onlineIcon}/>
-                  </div>
-                ))}
-              </h2>
+            <div id='main'>
+              <div id='container'>
+                <Tools/>
+                <WhiteBoard />
+              </div>
+              <History/>
             </div>
-          </div>
         )
         : null
     }
